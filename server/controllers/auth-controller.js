9c308ac6d22bde2b5fad0202a86aaa100784e372
Mihirnaik17 <mihir170204naik@gmil.com>
2025-11-03 15:36:38 -0500

    refactored auth-controller code

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,5 +1,5 @@
 const auth = require('../auth')
-const User = require('../models/user-model')
+const dbManager = require('../db')
 const bcrypt = require('bcryptjs')
 
 getLoggedIn = async (req, res) => {
@@ -13,7 +13,7 @@ getLoggedIn = async (req, res) => {
             })
         }
 
-        const loggedInUser = await User.findOne({ _id: userId });
+        const loggedInUser = await dbManager.getUserById(userId);
         console.log("loggedInUser: " + loggedInUser);
 
         return res.status(200).json({
@@ -41,7 +41,7 @@ loginUser = async (req, res) => {
                 .json({ errorMessage: "Please enter all required fields." });
         }
 
-        const existingUser = await User.findOne({ email: email });
+        const existingUser = await dbManager.getUserByEmail(email);
         console.log("existingUser: " + existingUser);
         if (!existingUser) {
             return res
@@ -63,7 +63,7 @@ loginUser = async (req, res) => {
         }
 
         // LOGIN THE USER
-        const token = auth.signToken(existingUser._id);
+        const token = auth.signToken(existingUser.id || existingUser._id);
         console.log(token);
 
         res.cookie("token", token, {
@@ -121,7 +121,7 @@ registerUser = async (req, res) => {
                 })
         }
         console.log("password and password verify match");
-        const existingUser = await User.findOne({ email: email });
+        const existingUser = await dbManager.getUserByEmail(email);
         console.log("existingUser: " + existingUser);
         if (existingUser) {
             return res
@@ -137,12 +137,13 @@ registerUser = async (req, res) => {
         const passwordHash = await bcrypt.hash(password, salt);
         console.log("passwordHash: " + passwordHash);
 
-        const newUser = new User({firstName, lastName, email, passwordHash});
-        const savedUser = await newUser.save();
-        console.log("new user saved: " + savedUser._id);
+        //const newUser = new User({firstName, lastName, email, passwordHash});
+        //const savedUser = await newUser.save();
+        const savedUser = await dbManager.createUser({firstName, lastName, email, passwordHash});
+        console.log("new user saved: " + (savedUser.id || savedUser._id));
 
         // LOGIN THE USER
-        const token = auth.signToken(savedUser._id);
+        const token = auth.signToken(savedUser.id || savedUser._id);
         console.log("token:" + token);
 
         await res.cookie("token", token, {
